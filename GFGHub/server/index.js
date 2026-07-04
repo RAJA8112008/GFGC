a21c990cfc7d2adf18b2cd4eb03ab62fc55b0be9
Raja <kraj9380286@gmail.com>
2026-07-04 16:24:54 +0530

    Update backend URL

--- GFGHub/server/index.js
+++ GFGHub/server/index.js
@@ -13,10 +13,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://gfgc-theta.vercel.app"
+        ],
+        credentials:true
+    })
+)
 app.use(express.json());
 
 // Connect to MongoDB
